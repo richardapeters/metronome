342a72b693b3cf3a7534d5dc43a6e010be437615
Richard Peters <richard@rpeters.org>
2020-07-31 09:52:07 +0200

    Add local_hal/stm32fxxx/SaiStm

---
+++ local_hal/stm32fxxx/SaiStm.cpp
@@ -0,0 +1,62 @@
+#include "generated/stm32fxxx/PeripheralTable.hpp"
+#include "local_hal/stm32fxxx/SaiStm.hpp"
+#include <cassert>
+
+#if defined(HAS_PERIPHERAL_SAI)
+
+namespace hal
+{
+    SaiA saiA;
+    SaiB saiB;
+
+    SaiStm::SaiStm(uint8_t oneBasedinstance, SaiA, GpioPinStm& mclock, GpioPinStm& sck, GpioPinStm& sd, GpioPinStm& fs, DmaStm& dma)
+        : instance(oneBasedinstance - 1)
+        , mclock(mclock, PinConfigTypeStm::saiMClock, oneBasedinstance)
+        , sck(sck, PinConfigTypeStm::saiSck, oneBasedinstance)
+        , sd(sd, PinConfigTypeStm::saiSd, oneBasedinstance)
+        , fs(fs, PinConfigTypeStm::saiFs, oneBasedinstance)
+        , saiBlockHandle(reinterpret_cast<SAI_Block_TypeDef*>(peripheralSai[instance] + 1))
+        , tx(dma, DmaChannelId{ 2, 4, 3 }, &saiBlockHandle->DR, [this]() {})    // SAI 2A
+    {
+        Init();
+    }
+
+    SaiStm::SaiStm(uint8_t oneBasedinstance, SaiB, GpioPinStm& mclock, GpioPinStm& sck, GpioPinStm& sd, GpioPinStm& fs, DmaStm& dma)
+        : instance(oneBasedinstance - 1)
+        , mclock(mclock, PinConfigTypeStm::saiMClock, oneBasedinstance)
+        , sck(sck, PinConfigTypeStm::saiSck, oneBasedinstance)
+        , sd(sd, PinConfigTypeStm::saiSd, oneBasedinstance)
+        , fs(fs, PinConfigTypeStm::saiFs, oneBasedinstance)
+        , saiBlockHandle(reinterpret_cast<SAI_Block_TypeDef*>(peripheralSai[instance] + 1) + 1)
+        , tx(dma, DmaChannelId{ 2, 6, 3 }, &saiBlockHandle->DR, [this]() {})    // SAI 2B
+    {
+        Init();
+    }
+
+    SaiStm::~SaiStm()
+    {
+        DisableClockSai(instance);
+    }
+
+    void SaiStm::Transfer(infra::MemoryRange<const uint16_t> data)
+    {
+        tx.StartTransmit(infra::ReinterpretCastByteRange(data));
+        saiBlockHandle->CR1 |= SAI_xCR1_DMAEN;
+    }
+
+    void SaiStm::Init()
+    {
+        tx.SetDataSize(2);
+
+        EnableClockSai(instance);
+
+        saiBlockHandle->CR1 = SAI_xCR1_MONO | SAI_xCR1_OUTDRIV | SAI_xCR1_DS_2;
+        saiBlockHandle->CR2 = 0;
+        saiBlockHandle->FRCR = SAI_xFRCR_FSOFF | SAI_xFRCR_FSDEF | (15 << 8) | 31;
+        saiBlockHandle->SLOTR = (3 << 16) | (1 << 8);
+
+        saiBlockHandle->CR1 |= SAI_xCR1_SAIEN;
+    }
+}
+
+#endif
---
+++ local_hal/stm32fxxx/SaiStm.hpp
@@ -0,0 +1,46 @@
+#ifndef SAI_STM_HPP
+#define SAI_STM_HPP
+
+#include "generated/stm32fxxx/PeripheralTable.hpp"
+#include "hal_st/stm32fxxx/DmaStm.hpp"
+#include "hal_st/stm32fxxx/GpioStm.hpp"
+#include "stm32f7xx.h"
+#include "stm32f7xx_hal.h"
+
+#if defined(HAS_PERIPHERAL_SAI)
+
+namespace hal
+{
+    struct SaiA {};
+    struct SaiB {};
+
+    extern SaiA saiA;
+    extern SaiB saiB;
+
+    class SaiStm
+    {
+    public:
+        SaiStm(uint8_t oneBasedIndex, SaiA, GpioPinStm& mclock, GpioPinStm& sck, GpioPinStm& sd, GpioPinStm& fs, DmaStm& dma);
+        SaiStm(uint8_t oneBasedIndex, SaiB, GpioPinStm& mclock, GpioPinStm& sck, GpioPinStm& sd, GpioPinStm& fs, DmaStm& dma);
+        ~SaiStm();
+
+        void Transfer(infra::MemoryRange<const uint16_t> data);
+
+    private:
+        void Init();
+
+    private:
+        uint8_t instance;
+        PeripheralPinStm mclock;
+        PeripheralPinStm sck;
+        PeripheralPinStm sd;
+        PeripheralPinStm fs;
+
+        SAI_Block_TypeDef* saiBlockHandle;
+        hal::DmaStm::Stream tx;
+    };
+}
+
+#endif
+
+#endif
