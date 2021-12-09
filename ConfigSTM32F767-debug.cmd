14b9359cf0a1439de5df029da43a0638394d4687
Richard Peters <richard@rpeters.org>
2021-12-09 12:56:04 +0100

    ConfigSTM32*.cmd: Fix for Visual Studio 2019

--- ConfigSTM32F767-debug.cmd
+++ ConfigSTM32F767-debug.cmd
@@ -1,5 +1,5 @@
 set path=%path:"=%
-call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat"
+call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
 mkdir ..\Metronome-Build-STM32F767-debug
 pushd ..\Metronome-Build-STM32F767-debug
 cmake ../Metronome -DCCOLA_DIR="../Metronome/EmbeddedInfraLib/ccola" -DCCOLA_INSTALL_DIR="../Metronome-Install" -DCMAKE_TOOLCHAIN_FILE="../Metronome/EmbeddedInfraLib/ccola/toolchain-gcc-arm-cubeide.cmake" -G"Eclipse CDT4 - NMake Makefiles" -DCMAKE_ECLIPSE_VERSION=4.6 -DCMAKE_SYSTEM_PROCESSOR="stm32f767" -DCMAKE_ECLIPSE_MAKE_ARGUMENTS=-s -DCCOLA_EXTRA_CONFIGURATION=debug
