e681a5228368ff3a3d3308dcac55faede829e568
Richard Peters <richard.peters@philips.com>
2023-02-02 16:37:31 +0100

    Rename submodules

--- toolchain-arm-gcc.cmake
+++ toolchain-arm-gcc.cmake
@@ -1,5 +1,3 @@
-include(CMakeForceCompiler)
-
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
@@ -8,18 +6,18 @@ set(hints_paths)
 
 if (CMAKE_HOST_WIN32)
     set(X86_PROGRAM_FILES "ProgramFiles(x86)")
-    list(APPEND hints_paths "$ENV{${X86_PROGRAM_FILES}}/GNU Tools Arm Embedded/8 2019-q3-update/bin/")
-    file(GLOB bin_paths "C:/ST/STM32CubeIDE_*/STM32CubeIDE/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.*/tools/bin/")
-    list(APPEND hints_paths ${bin_paths})
+    list(APPEND hints_paths "$ENV{${X86_PROGRAM_FILES}}/GNU Arm Embedded Toolchain/10 2021.10/bin/")
 endif()
 
 find_program(CMAKE_C_COMPILER
     NAMES arm-none-eabi-gcc
-    HINTS ${hints_paths})
+    HINTS ${hints_paths}
+    REQUIRED)
 
 find_program(CMAKE_CXX_COMPILER
     NAMES arm-none-eabi-g++
-    HINTS ${hints_paths})
+    HINTS ${hints_paths}
+    REQUIRED)
 
 find_program(CMAKE_ASM_COMPILER
     NAMES arm-none-eabi-as
@@ -37,28 +35,36 @@ find_program(CMAKE_NM
     NAMES arm-none-eabi-nm
     HINTS ${hints_paths})
 
-find_program(CCOLA_LD_TOOL
+find_program(EMIL_LD_TOOL
     NAMES arm-none-eabi-ld
     HINTS ${hints_paths})
 
-find_program(CCOLA_APP_SIZE_TOOL
+find_program(EMIL_APP_SIZE_TOOL
     NAMES arm-none-eabi-size
     HINTS ${hints_paths})
 
-find_program(CCOLA_OBJ_COPY_TOOL
+find_program(EMIL_OBJ_COPY_TOOL
     NAMES arm-none-eabi-objcopy
     HINTS ${hints_paths})
 
-find_program(CCOLA_OBJ_DUMP_TOOL
+find_program(EMIL_OBJ_DUMP_TOOL
     NAMES arm-none-eabi-objdump
     HINTS ${hints_paths})
 
-find_program(CCOLA_DEBUG_TOOL
+find_program(EMIL_DEBUG_TOOL
     NAMES arm-none-eabi-gdb
     HINTS ${hints_paths})
 
-add_link_options(LINKER:--gc-sections,--print-memory-usage)
-add_link_options(-specs=nano.specs -nostartfiles -mthumb -mcpu=cortex-m7)
+if (TARGET_MCU_FAMILY STREQUAL stm32f7xx)
+    set(cpu cortex-m7)
+elseif (TARGET_MCU_FAMILY STREQUAL stm32f4xx)
+    set(cpu cortex-m4)
+else()
+    set(cpu cortex-m0plus)
+endif()
 
-add_compile_options(-ffunction-sections -fdata-sections -mthumb -mcpu=cortex-m7 -g3)
+add_link_options(LINKER:--gc-sections,--print-memory-usage)
+add_link_options(-specs=nano.specs -nostartfiles -mthumb -mcpu=${cpu})
+
+add_compile_options(-ffunction-sections -fdata-sections -mthumb -mcpu=${cpu})
 add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-fno-use-cxa-atexit$<SEMICOLON>-fno-threadsafe-statics$<SEMICOLON>-fno-rtti$<SEMICOLON>-fno-exceptions>)
